c3a8098bf4756a7aa615a500a0f44ef486c30c4a
AdamMenard <adammenard888@gmail.com>
2018-03-01 20:55:13 -0800

    commit for code review

--- src/components/myLists.js
+++ src/components/myLists.js
@@ -5,9 +5,11 @@ class MyLists extends Component {
   constructor() {
     super();
     this.state = {
-      movieLists: []
+      movieLists: [],
+      editedMovieListTitle: []
     }
     this.onDeleteMovieList = this.onDeleteMovieList.bind(this);
+    this.onEditMovieList = this.onEditMovieList.bind(this);
   }
   componentWillMount() {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/api/movie_lists`).then((res) => {
@@ -33,13 +35,31 @@ class MyLists extends Component {
 		    return res.json()
 		}).then((json) => {
       let remainingMovies = this.state.movieLists.filter(function(eachList) {
-              return eachList._id !== json._id
-            });
-            this.setState({ movieLists: remainingMovies})
+          return eachList._id !== json._id
+        });
+        this.setState({ movieLists: remainingMovies})
 		});
   }
-  onEditMovieList() {
+  onEditMovieList(e, eachMovieListId) {
     console.log('Edit button Clicked')
+    let newMovieListTitle = this.state.editedMovieListTitle
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/api/movie_lists/${ eachMovieListId }`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: newMovieListTitle
+      }),
+    }).then((res) => {
+        return res.json()
+    }).then((json) => {
+      newMovieListTitle.update(function(eachList) {
+          // return
+        });
+        this.setState({ movieLists: newMovieListTitle})
+    });
   }
   render() {
     return (
@@ -51,11 +71,11 @@ class MyLists extends Component {
             <div id="movieList">
               <Link to={`/communityMemberSingleList/${ eachMovieList._id }`}>
                 <h4>{ eachMovieList.title }</h4></Link>
-              <button class='btn btn-info'
+              <button class="btn btn-info"
                       onClick={e => this.onEditMovieList(e, eachMovieList._id)}>Edit:{ eachMovieList.title }</button>
-              {/* && operator checks for 'eachMovieList._id' to load before displaying delete button */}
+              {/* COMMENT: && operator checks for 'eachMovieList._id' to load before displaying delete button */}
               {eachMovieList._id &&
-                <button className="btn btn-danger"
+              <button className="btn btn-danger"
                       onClick={e => this.onDeleteMovieList(e, eachMovieList._id)}>Delete Movie List</button>
                     }
             </div>
